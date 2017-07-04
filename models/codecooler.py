cbf5afe9d924b54274800809d37564e9272c3245
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-07-04 12:21:26 +0200

    Add attributes of codecooler

--- models/codecooler.py
+++ models/codecooler.py
@@ -1,2 +1,8 @@
 class Codecooler:
-    pass
+    
+    def __init__(self, name, surname, login, password, email):
+	self.name = name
+	self.surname = surname
+	self.login = login
+	self.password = password
+	self.email = email
