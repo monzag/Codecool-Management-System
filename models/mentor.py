0ec8212b642b70ba0857c65ec49614f54a767151
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-07-04 12:30:15 +0200

    add mentor init

--- models/mentor.py
+++ models/mentor.py
@@ -2,4 +2,9 @@ from employee import Employee
 
 
 class Mentor(Employee):
-    pass
+
+    list_of_mentors = []
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        Employee.list_of_mentors.append(self)
