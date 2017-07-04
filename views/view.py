530f30011f52e26761b23ee813dd8a1f04123807
wutismyname <plebejoosh@gmail.com>
2017-07-04 15:09:43 +0200

    Added menu, input and error printing

---
+++ views/view.py
@@ -0,0 +1,33 @@
+def print_menu(title, options, exit_message):
+
+    option_number = 1
+
+    print('{}:'.format(title))
+    for option in options:
+        print('    ({}) '.format(option_number) + option)
+        option_number += 1
+    print('    (0) {}'.format(exit_message))
+    print('')
+
+
+def print_table():
+    pass
+
+
+def get_inputs(labels, title):
+
+    inputs = []
+
+    print(title)
+    for label in labels:
+        answer = (input('{} '.format(label)))
+        inputs.append(answer)
+    print('')
+
+    return inputs
+
+
+def print_error_message(message):
+
+    print(message)
+    print('')
