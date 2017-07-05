4fc500e9cb8ea060559b367d14e47ae767a4e7c1
monzag <zaganiacz.m@gmail.com>
2017-07-05 10:24:38 +0200

    Add new attributes: attendance, assignment

--- models/student.py
+++ models/student.py
@@ -5,9 +5,11 @@ class Student(Codecooler):
 
     list_of_students = []
 
-    def __init__(self, *args):
+    def __init__(self, attendance, assignment, *args):
         super().__init__(*args)
         Student.list_of_students.append(self)
-    
+        self.attendance = attendance
+        self.assignment = assignment
+
     def __str__(self):
         return '{} {}'.format(self.name, self.surname)
