893b1a4b3dd57febd1c7084ad6d41df05de5eea2
monzag <zaganiacz.m@gmail.com>
2017-07-05 21:12:28 +0200

    Add load_data_from_file

--- models/student.py
+++ models/student.py
@@ -5,11 +5,39 @@ class Student(Codecooler):
 
     list_of_students = []
 
-    def __init__(self, attendance, assignment, *args):
+    def __init__(self, *args):
         super().__init__(*args)
         Student.list_of_students.append(self)
-        self.attendance = attendance
-        self.assignment = assignment
+        '''self.attendance = attendance
+        self.assignments_list = assignments.list'''
 
-    def __str__(self):
-        return '{} {}'.format(self.name, self.surname)
+    @classmethod
+    def load_students_from_file(self):
+        '''
+        Read students data from csv file.
+        Raise error if file not exist.
+
+        Returns:
+            splitted - list of lists
+        '''
+
+        filename = 'students.csv'
+        if not os.path.exists(filename):
+            raise FileNotFoundError("There is no such a file")
+
+        else:
+            with open(filename, 'r') as csvfile:
+                read_data = csvfile.readlines()
+                splitted = [line.replace('\n', '').split('|') for line in read_data]
+
+        return splitted
+
+    '''def save_data_to_file(self, list_to_save):
+
+        Save students data to csv file. If file not exist, create file.
+
+
+        filename = 'students.csv'
+        with open(filename, 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(list_to_save)'''
