31d721105c356930e919ab1f180e5b2796129a72
wutismyname <plebejoosh@gmail.com>
2017-07-05 11:15:36 +0200

    Added finding longest string for table printing

--- views/view.py
+++ views/view.py
@@ -5,6 +5,16 @@ import getpass
 
 
 def print_menu(title, options, exit_message):
+    '''Prints the menu options.
+
+    Args:
+        title (str) - title of the menu
+        options (list) - list of strings as option names
+        exit_message (str) - string to be printed as the last option to quit
+
+    Returns:
+        Nothing, it just prints the menu in the console.
+    '''
 
     option_number = 1
 
@@ -16,22 +26,41 @@ def print_menu(title, options, exit_message):
     print('')
 
 
-def print_table():
-    pass
+def print_table(table, title_list):
+    '''Prints the table with provided data.
+
+    Args:
+        table (list) - list of lists with data to be printed
+        title_list (list) - list of column titles to be printed at the top
+
+    Returns:
+        Nothing, it just prints the table in the console.
+        If table is empty or its data is invalid, then returns None.
+    '''
+
+    if is_table_wrong(table, title_list):
+        return None
+
+    MIN_COLUMN_WIDTH = 8
+    CELL_PADDING = 2
+    columns_number = len(table[0])
+
+    outer_row = create_outer_row(table, columns_number, title_list, MIN_COLUMN_WIDTH, CELL_PADDING)
 
 
-def error_check(table, title_list):
+def is_table_wrong(table, title_list):
     '''
     Checks if table is empty, has different amount of entries in lists
     or has different amount of entries between lists and titles.
 
     Args:
-        table: list of lists with data
-        title_list: list containing table headers
+        table (list) - list of lists with data
+        title_list (list) - list containing table headers
 
     Returns:
-        boolean
+        bool
     '''
+
     if table == []:
         print_message("The table is empty! Check if CSV exists in a current folder.")
         return True
@@ -46,8 +75,71 @@ def error_check(table, title_list):
     return False
 
 
-def get_inputs(labels, title):
+def find_max_string_length(table, item_index, title_list):
+    '''
+    Finds longest string from all items of a given column index.
 
+    Args:
+        table (list) - list of lists of all the strings
+        item_index (int) - specific index in lists in table
+        title_list (list) - list containing table headers
+
+    Returns:
+        int - longest length value for a given index
+    '''
+
+    longest_string = ''
+
+    for a_list in table:
+        if len(str(a_list[item_index])) > len(longest_string):
+            longest_string = str(a_list[item_index])
+
+    if len(str(title_list[item_index])) > len(longest_string):
+        longest_string = str(title_list[item_index])
+
+    return len(longest_value)
+
+
+def create_outer_row(table, columns_number, title_list, MIN_COLUMN_WIDTH, CELL_PADDING):
+    '''
+    Generates a string to be later printed as an outer row in a table.
+
+    Args:
+        table (list) - list of lists of all the strings
+        columns_number (int) - number of columns
+        title_list (list) - list containing table headers
+        MIN_COLUMN_WIDTH (int)
+        CELL_PADDING (int)
+
+    Returns:
+        outer_row (str) - string ready to be printed
+    '''
+
+    outer_row = ''
+
+    for column in range(columns_number):
+        dashes_to_add = find_max_string_length(table, column, title_list)
+        if dashes_to_add >= MIN_COLUMN_WIDTH:
+            outer_row = outer_row + ('-' * (dashes_to_add + CELL_PADDING))
+        else:
+            outer_row = outer_row + ('-' * MIN_COLUMN_WIDTH)
+
+    additional_dashes = columns_number - 1
+    outer_row = outer_row + ('-' * additional_dashes)
+
+    return outer_row
+
+
+def get_inputs(labels, title):
+    '''Prints input prompts and gets inputs.
+
+    Args:
+        labels (list) - list containing input details
+        title (str) - title of input prompt
+
+    Returns:
+        inputs (list) - list with inputs received
+    '''
     inputs = []
 
     print(title)
