3703b6530abca32903e60a969b10c38aa45a7b49
wutismyname <plebejoosh@gmail.com>
2017-07-05 09:42:20 +0200

    Added welcome logo

--- views/view.py
+++ views/view.py
@@ -1,3 +1,28 @@
+import os
+import sys
+import termios
+
+def wait_until_key_pressed():
+    ''' Waits for a key press on the console and returns it. '''
+
+    result = None
+    fd = sys.stdin.fileno()
+
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+    try:
+        result = sys.stdin.read(1)
+    except IOError:
+        pass
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+
+    return result
+
+
 def print_menu(title, options, exit_message):
 
     option_number = 1
@@ -34,7 +59,35 @@ def print_message(message):
 
 
 def print_welcome_screen():
-    print('''''')
-    
-def view.wait_until_key_pressed():
-    input("PRESS ANY KEY TO CONTINUE")
+    print('''
+
+                                           ``-://++ooo/.
+                                         -o+++ssssooooss+:`
+                                       `/++///+ososssosy++o+-`
+                                      -o++++o+:.`  `-:+so+//+o-
+                                     /sssyy+            /+//+o+
+                                    /soooss`             /oossy.
+                                    :sooss-               +ysss:
+                                    `sssys`              `ssoos+
+                                     osso+o`             osoooso
+                                     :o+//+o`          `:yysss+
+                                     `+++/+oyo/:.` `-:+o++++o-
+                                       .:+++ssoosssys++//+o/`
+                                          -/syoooosssso++o-
+                                            `-+o+//::--``
+
+
+
+
+     `:+oo+:     -/+oo+:`    /++++/:`    :+++++.    `:+oo+:     .:+oo+:`      -/+oo/-     /+.
+    /hy/--:.    ohs:.-+hy-   sho--/yy:   +hs---`   -yy+--::    +hy:../yy:   `shs-.-ohy.   sh:
+    hh:        :hh`    ohs   sh+   /hy   +hyyyo    sh+        `hh:    /hh   :hh     sh+   sh:
+    ohy-` ``   `yh+`  :yh:   sh+``-yh+   +hs       /hy:` `.    ohs.  .yh+   .yh/` `:hy-   sh:
+     :osyyy+    `:oyyys+.    oyyyso+-    /yyyyy/    -+syyys`    :osyyso:     `/oyyys+.    oyyyyy
+
+
+                                     PRESS ANY KEY TO CONTINUE
+     ''')
+
+print_welcome_screen()
+wait_until_key_pressed()
