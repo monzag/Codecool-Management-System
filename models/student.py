61afef07c33ae8de5051c0faeb8edd88a93d83b6
monzag <zaganiacz.m@gmail.com>
2017-07-05 22:53:45 +0200

    Add convert_list_of_object_to_data, fix save_data_to_file

--- models/student.py
+++ models/student.py
@@ -56,12 +56,31 @@ class Student(Codecooler):
         new_student = Student(name, surname, login, password, email)
         return new_student
 
-    '''def save_data_to_file(self, list_to_save):
-
+    @classmethod
+    def save_data_to_file(cls):
+        '''
         Save students data to csv file. If file not exist, create file.
-
-
+        '''
+        list_to_save = Student.convert_list_of_object_to_data()
         filename = 'students.csv'
-        with open(filename, 'a') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerows(list_to_save)'''
+        with open(filename, 'w') as csvfile:
+            for record in list_to_save:
+                row = ','.join(record)
+                csvfile.write(row + "\n")
+
+    @classmethod
+    def convert_list_of_object_to_data(cls):
+        '''
+        Unpack attributes of Student object as data to student_data list and add it to list_to_save.
+
+        Returns:
+            list_to_save - list of lists
+        '''
+
+        list_to_save = []
+        for student in Student.list_of_students:
+            name, surname, login, password, email = student.name, student.surname, student.login, student.password, student.email
+            student_data = [name, surname, login, password, email]
+            list_to_save.append(student_data)
+
+        return list_to_save
