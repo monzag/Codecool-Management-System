b14a94c319aed7d635169b1a93dedccd9bf4865f
monzag <zaganiacz.m@gmail.com>
2017-07-05 22:09:26 +0200

    Add create new_student

--- models/student.py
+++ models/student.py
@@ -1,4 +1,5 @@
 from models.codecooler import Codecooler
+import os
 
 
 class Student(Codecooler):
@@ -12,7 +13,7 @@ class Student(Codecooler):
         self.assignments_list = assignments.list'''
 
     @classmethod
-    def load_students_from_file(self):
+    def load_students_from_file(cls):
         '''
         Read students data from csv file.
         Raise error if file not exist.
@@ -28,9 +29,32 @@ class Student(Codecooler):
         else:
             with open(filename, 'r') as csvfile:
                 read_data = csvfile.readlines()
-                splitted = [line.replace('\n', '').split('|') for line in read_data]
+                splitted = [line.replace('\n', '').split(',') for line in read_data]
 
         return splitted
+
+    @classmethod
+    def get_students_from_file(cls):
+        '''
+        Get data all students and create each Student object (automatically append to list_of_students)
+        '''
+
+        data_all_students = Student.load_students_from_file()
+        for student in data_all_students:
+            name, surname, login, password, email = student[0], student[1], student[2], student[3], student[4]
+            Student.create_new_student(name, surname, login, password, email)
+
+    @classmethod
+    def create_new_student(cls, name, surname, login, password, email):
+        '''
+        Create new object Student.
+
+        Returns:
+            new_student - obj
+        '''
+
+        new_student = Student(name, surname, login, password, email)
+        return new_student
 
     '''def save_data_to_file(self, list_to_save):
 
