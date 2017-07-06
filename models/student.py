96c821c6faa829adb2da81720a4748beea3a689a
monzag <zaganiacz.m@gmail.com>
2017-07-06 10:00:14 +0200

    Add attribute attendance, add get_codecooler_from_file

--- models/student.py
+++ models/student.py
@@ -6,18 +6,35 @@ class Student(Codecooler):
 
     list_of_students = []
 
-    def __init__(self, *args):
+    def __init__(self, attendance, *args):
         super().__init__(*args)
         Student.list_of_students.append(self)
-        '''self.attendance = attendance
-        self.assignments_list = assignments.list'''
+        self.attendance = attendance
+        '''self.assignments_list = assignments.list'''
+
+    @classmethod
+    def get_codecoolers_from_file(cls, file_name):
+        """
+        Creates objects with data from splitted list.
+
+        Returns:
+                None
+        """
+
+        splitted_data_list = cls.load_data_from_file(file_name)
+        print(splitted_data_list)
+
+        for element in splitted_data_list:
+            name, surname, login, password, mail, attendance = element
+            attendance = int(attendance)
+            cls(attendance, name, surname, login, password, mail)
 
     @classmethod
     def save_data_to_file(cls):
         '''
         Save students data to csv file. If file not exist, create file.
         '''
-        list_to_save = Student.convert_list_of_object_to_data()
+        list_to_save = cls.convert_list_of_object_to_data()
         filename = 'students.csv'
         with open(filename, 'w') as csvfile:
             for record in list_to_save:
@@ -34,7 +51,7 @@ class Student(Codecooler):
         '''
 
         list_to_save = []
-        for student in Student.list_of_students:
+        for student in cls.list_of_students:
             name, surname, login, password, email = student.name, student.surname, student.login, student.password, student.email
             student_data = [name, surname, login, password, email]
             list_to_save.append(student_data)
