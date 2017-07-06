0d3e6f0d5a5aa94485cecb16ea1f2c359c0fe786
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-07-06 02:21:12 +0200

    Add list_of_mentors

--- models/manager.py
+++ models/manager.py
@@ -1,7 +1,10 @@
-from employee import Employee
+from models.employee import Employee
 
 
 class Manager(Employee):
 
+    list_of_managers = []
+
     def __init__(self, *args):
         super().__init__(*args)
+        Manager.list_of_mentors.append(self)
