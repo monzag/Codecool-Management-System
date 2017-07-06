36ec2459eb27087ac9afd75aea5bb2579ada1369
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-07-06 02:12:21 +0200

    Change student to codecooler in docs and variables

--- models/codecooler.py
+++ models/codecooler.py
@@ -7,40 +7,39 @@ class Codecooler:
 		self.password = password
 		self.email = email
 
-    @classmethod
-    def load_students_from_file(cls):
+    @staticmethod
+    def load_codecoolers_from_file(file_name):
         '''
-        Read students data from csv file.
+        Read Codecooler obj data from csv file.
         Raise error if file not exist.
 
         Returns:
             splitted - list of lists
         '''
 
-        file_path = os.getcwd() + '/data/students.csv'
+        file_path = os.getcwd() + '/data/' + file_name
         if not os.path.exists(file_path):
             raise FileNotFoundError("There is no such a file")
 
         else:
-            with open(filename, 'r') as csvfile:
+            with open(file_path, 'r') as csvfile:
                 read_data = csvfile.readlines()
                 splitted = [line.replace('\n', '').split(',') for line in read_data]
 
         return splitted
 
     @classmethod
-    def get_students_from_file(cls):
+    def get_codecooler_from_file(cls, file_name):
         '''
-        Get data all students and create each Student object (automatically append to list_of_students)
+        Get data all codecoolers and create each Codecooler object (automatically append to list_of_codecoolers)
         '''
-
-        data_all_students = Student.load_students_from_file()
-        for student in data_all_students:
-            name, surname, login, password, email = student[0], student[1], student[2], student[3], student[4]
-            Student.create_new_student(name, surname, login, password, email)
+        data_all_codecoolers = cls.load_codecoolers_from_file(file_name)
+        for codecooler in data_all_codecoolers:
+            name, surname, login, password, email = codecooler[0], codecooler[1], codecooler[2], codecooler[3], codecooler[4]
+            cls.create_new_codecooler(name, surname, login, password, email)
 
     @classmethod
-    def create_new_student(cls, name, surname, login, password, email):
+    def create_new_codecooler(cls, name, surname, login, password, email):
         '''
         Create new object Student.
 
@@ -50,32 +49,3 @@ class Codecooler:
 
         new_student = Student(name, surname, login, password, email)
         return new_student
-
-    @classmethod
-    def save_data_to_file(cls):
-        '''
-        Save students data to csv file. If file not exist, create file.
-        '''
-        list_to_save = Student.convert_list_of_object_to_data()
-        filename = 'students.csv'
-        with open(filename, 'w') as csvfile:
-            for record in list_to_save:
-                row = ','.join(record)
-                csvfile.write(row + "\n")
-
-    @classmethod
-    def convert_list_of_object_to_data(cls):
-        '''
-        Unpack attributes of Student object as data to student_data list and add it to list_to_save.
-
-        Returns:
-            list_to_save - list of lists
-        '''
-
-        list_to_save = []
-        for student in Student.list_of_students:
-            name, surname, login, password, email = student.name, student.surname, student.login, student.password, student.email
-            student_data = [name, surname, login, password, email]
-            list_to_save.append(student_data)
-
-        return list_to_save
