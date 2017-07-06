b2388f8c57ba7d3ea2b81c7e1cc5a3a7af6f1780
monzag <zaganiacz.m@gmail.com>
2017-07-06 18:22:23 +0200

    Remove os

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -1,5 +1,3 @@
-import os
-
 from views import view
 from controllers import assignment_controller
 
@@ -22,7 +20,6 @@ def student_menu(user):
 
     end = False
     while not end:
-        os.system('clear')
 
         view.print_menu(title, options, exit_message)
         option = view.input_number()
