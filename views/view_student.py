1f92aad6d669500283311734d1fd8fc3ac018835
monzag <zaganiacz.m@gmail.com>
2017-07-18 11:23:07 +0200

    Add data_to_student_menu and title to view_grades

---
+++ views/view_student.py
@@ -0,0 +1,12 @@
+def data_to_student_menu(user):
+    title = 'Hi {}! What would you like to do'.format(user.name)
+    exit_message = 'Exit'
+    options = ['View grades', 'Submit assigment']
+
+    return title, exit_message, options
+
+
+def title_to_view_grades():
+    title_list = ['Assignment', 'status', 'submit_date', 'deadline', 'grade', 'max_grade']
+
+    return title_list
