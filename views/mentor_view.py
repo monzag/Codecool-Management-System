48336ad24534b514ab35db3eb6aaaad71a82b460
wutismyname <plebejoosh@gmail.com>
2017-07-19 15:15:06 +0200

    Add menu_labels to mentor_view

---
+++ views/mentor_view.py
@@ -0,0 +1,6 @@
+def menu_labels(user):
+
+    title = 'Hi {}! What would you like to do'.format(user.name)
+    exit_message = 'Exit'
+    options = ['View students', 'Add assignment', 'Grade assignment',
+               'Check attendance', 'Add student', 'Remove student']
