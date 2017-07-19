e1fe5bf5c416a55d7154be9453e1e2dd2505f042
wutismyname <plebejoosh@gmail.com>
2017-07-19 15:15:37 +0200

    Add data_to_check_attendance to mentor_view

--- views/mentor_view.py
+++ views/mentor_view.py
@@ -4,3 +4,10 @@ def menu_labels(user):
     exit_message = 'Exit'
     options = ['View students', 'Add assignment', 'Grade assignment',
                'Check attendance', 'Add student', 'Remove student']
+def data_to_check_attendance():
+
+    title = "Student attendance for today"
+    exit_message = 'Back to Main Menu (NOTICE: You will have to check whole attendance again for today!)'
+    options = ['Present', 'Late', 'Absent']
+
+    return title, exit_message, options
