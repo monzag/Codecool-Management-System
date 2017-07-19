479052d6e94bd25ab1a9768ddf32912db40dfb83
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-07-19 12:43:19 +0200

    Add from_file classmethod

--- models/assignment.py
+++ models/assignment.py
@@ -1,74 +1,51 @@
 import os
 
+from models.solution import Solution
 
 class Assignment:
 
-    # all ssigments ever created and match with student
     list_of_assignments = []
 
-    def __init__(self, login, name, add_date, deadline, max_grade, grade=0, status='undone', submit_date='not submitted'):
+    def __init__(self, name, add_date, deadline, max_grade):
         self.name = name
-        self.owner = login
-        self.status = status
         self.add_date = add_date
         self.deadline = deadline
-        self.submit_date = submit_date
-        self.grade = grade
         self.max_grade = max_grade
+        self.solutions = solutions
 
         Assignment.list_of_assignments.append(self)
 
-    @staticmethod
-    def load_data_from_file(file_name):
-        """
-        Loads Codecooler obj. instance information from csv file, splits them, and
-        creates a list.
-
-        Return:
-            list: list with Codecooler obj. instance data
-        """
-
-        file_path = os.getcwd() + '/data/' + file_name
-        if not os.path.exists(file_path):
-            raise FileNotFoundError("There is no such file")
-        else:
-            with open(file_path, 'r') as csvfile:
-                read_data = csvfile.readlines()
-                splitted_data_list = [line.replace('\n', '').split('|') for line in read_data]
-
-        return splitted_data_list
-
     @classmethod
-    def get_assignments_from_file(cls, file_name):
-        """
-        Creates objects with data from splitted list.
-
-        Returns:
-            None
-        """
-        splitted_data_list = cls.load_data_from_file(file_name)
-
-        for element in splitted_data_list:
-            name, owner, status, add_date, deadline, submit_date, grade, max_grade = element
-            grade = int(grade)
-            max_grade = int(max_grade)
-            cls(owner, name, add_date, deadline, max_grade, grade, status, submit_date)
-
-    @classmethod
-    def save_assignments_to_file(cls):
+    def from_folder(cls):
         '''
         '''
-        file_path = os.getcwd() + '/data/assignments.csv'
-        with open(file_path, 'w') as data:
-            data.write(cls.csv_string())
+        file_path = os.getcwd() + '/data/assigments/'
+        for file_name in os.listdir(file_path):
+            if file_name.endswith('.csv'):
+                cls.from_file(file_path + file_name)
+
+    def from_file(cls, file_path):
+        '''
+        '''
+        if os.path.exists(file_path):
+
+            with open(file_path, 'r') as data:
+                file_rows = data.read.split('$')
+
+            name, add_date, deadline, max_grade = file_rows[0].replace('\n', '').split('|')
+            solutions = cls.assign_solutions(file_rows)
+
+            cls(name, add_date, deadline, max_grade, solutions)
 
     @classmethod
-    def csv_string(cls):
-        csv_string = []
-        for assignment in cls.list_of_assignments:
-            row = [assignment.name, assignment.owner, assignment.status, assignment.add_date, 
-                   assignment.deadline, assignment.submit_date, str(assignment.grade), str(assignment.max_grade)]
+    def assign_solutions(cls, file_rows):
+        '''
+        '''
+        solutions = []
 
-            csv_string.append(row)
+        for row in file_rows[1:]:
+            row = row.replace('\n', '').split('|')
+            solution = Solutions.from_list(row)
+            solutions.append(solution)
 
-        return '\n'.join('|'.join(row) for row in csv_string)
+        return solutions
