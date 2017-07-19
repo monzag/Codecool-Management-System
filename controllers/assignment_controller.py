abebdca340ce637a21fdfe7ca9dedbdcdb5d36f2
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-07-19 18:15:34 +0200

    Add save_solution_to_file

--- controllers/assignment_controller.py
+++ controllers/assignment_controller.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from models.assignment import Assignment
 
+from views import assignment_view
+
 
 def get_assignments_to_table(student_index):
     '''
@@ -26,12 +28,26 @@ def submit_solution_to_assignment(assignment, student_index):
     '''
     solution = assignment.solutions[student_index]
 
-    if str(solution.grade) + solution.submit_date != '00':
+    if solution.submit_date == '0':
 
-        new_submit_date = '{}:{}:{}'.format(datetime.today().year, datetime.today().month, datetime.today().day)
-        solution.submit_date = new_submit_date
+        solution.submit_date = get_submit_date()
+        solution.file_name = solution.get_file_name(student_index, assignment.name)
+        save_solution_to_file(solution.file_name)
 
-        file_path = solution.get_file_name(student_index, assignment_name)
-        solution.file_name = file_path
+    else:
+        assignment_view.print_fail_message()
 
-        solution_text = assignment_view.get_solution_text()
+
+def get_submit_date():
+    '''
+    '''
+    return '{}:{}:{}'.format(datetime.today().year, datetime.today().month, datetime.today().day)
+
+
+def save_solution_to_file(file_name):
+    '''
+    '''
+    text = assignment_view.get_submision_text()
+
+    with open(file_name, 'w+') as solution_file:
+        solution_file.write(text)
