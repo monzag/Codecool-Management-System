8918929424efbf21f2d2e8f04ce8bfac58684633
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-07-19 16:41:14 +0200

    Refactor get_assignments_to_table function

--- controllers/assignment_controller.py
+++ controllers/assignment_controller.py
@@ -4,4 +4,16 @@ from models.assignment import Assignment
 def get_assignments_to_table(student_index):
     '''
     '''
-    pass
+    table = []
+    for assignment in Assignment.list_of_assignments:
+        solution = []
+        solution.append(assignment.name)
+        solution.append(assignment.add_date)
+        solution.append(assignment.deadline)
+        solution.append(assignment.max_grade)
+        solution.append(assignment.solutions[student_index].formated_submit_date)
+        solution.append(assignment.solutions[student_index].formated_grade)
+
+        table.append(solution)
+
+    return table
