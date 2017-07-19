965931c7418702b300d1cc25ba173bc29fe76195
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-07-19 13:04:40 +0200

    Add docstrings

--- views/manager_view.py
+++ views/manager_view.py
@@ -2,6 +2,14 @@ from string import punctuation
 
 
 def get_valid_input(function, text):
+    """
+    Get input from user and use check function to
+    validate input.
+
+    Return:
+            str: user input if input is valid
+            None: if user input is invalid
+    """
 
     correct = None
     while not correct:
@@ -11,6 +19,12 @@ def get_valid_input(function, text):
     return user_input
 
 def get_single_input(text):
+    """
+    Get input from user.
+
+    Return:
+            str: user input
+    """        
 
     user_input = input(text)
 
