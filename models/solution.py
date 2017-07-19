ee34442c407c9af2beaee1a47a1a77e1545188af
arkadiusz bystrowicz <arek.bystrowicz@gmail.com>
2017-07-19 17:01:24 +0200

    Fix grade not int bug

--- models/solution.py
+++ models/solution.py
@@ -48,14 +48,14 @@ class Solution:
 
     @property
     def formated_grade(self):
-        if grade > 0:
-            return str(grade)
+        if self.grade > 0:
+            return str(self.grade)
         else:
             return 'not graded yet'
 
     @property
     def formated_submit_date(self):
-        if submit_date != '0':
-            return submit_date
+        if self.submit_date != '0':
+            return self.submit_date
         else:
             return 'not submited yet'
