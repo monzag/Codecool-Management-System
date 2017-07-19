da1ee6bbd1a025e414cc08e5155261097cc9a190
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2017-07-19 15:05:31 +0200

    Add manager_menu_titles function

--- views/manager_view.py
+++ views/manager_view.py
@@ -18,14 +18,33 @@ def get_valid_input(function, text):
 
     return user_input
 
+
 def get_single_input(text):
     """
     Get input from user.
 
     Return:
             str: user input
-    """        
+    """
 
     user_input = input(text)
 
     return user_input
+
+
+def manager_menu_titles(user):
+    """
+    Holds string and list of string titles for printing manager_menu_titles
+    menu.
+
+    Return:
+            list: list of string titles
+            str: menu title
+            str: exit message
+    """
+
+    title = "Manager menu"
+    options = ['View students', 'View mentors', 'Add mentor', 'Remove mentor']
+    exit_message = "Exit"
+
+    return title, options, exit_message
