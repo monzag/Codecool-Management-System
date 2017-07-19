5e28ae710a3adf0aa151879b44ea539dc87d10b0
wutismyname <plebejoosh@gmail.com>
2017-07-19 11:16:25 +0200

    Add load_data_from_file in attendance

--- models/attendance.py
+++ models/attendance.py
@@ -12,3 +12,17 @@ class Attendance:
         self.date = date
         self.today_value = int(today_value)
         Attendance.list_of_attendance.append(self)
+    @staticmethod
+    def load_data_from_file(filename):
+
+        file_path = os.getcwd() + '/data/' + filename
+
+        if not os.path.exists(file_path):
+            raise FileNotFoundError("There is no such a file")
+
+        else:
+            with open(file_path, 'r') as csvfile:
+                read_data = csvfile.readlines()
+                splitted_data_list = [line.replace('\n', '').split('|') for line in read_data]
+
+        return splitted_data_list
