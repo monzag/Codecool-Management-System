f396574fa9527817476819f77e48a14cc37a6fc1
wutismyname <plebejoosh@gmail.com>
2017-07-20 13:52:07 +0200

    Add skip_student to check attendance

--- views/mentor_view.py
+++ views/mentor_view.py
@@ -12,19 +12,19 @@ def data_to_check_attendance():
 
     title = "Student attendance for today"
     exit_message = 'Back to Main Menu'
-    options = ['Present', 'Late', 'Absent']
+    options = ['Present', 'Late', 'Absent', 'Skip student']
 
     return title, exit_message, options
 
 
 def attendance_checked():
 
-    print("\nAll attendance for today is checked.\n")
+    print("\nAll attendance for today is checked or no students in database.\n")
 
 
 def get_data_to_add_student():
     '''
-    Get proper label when student is added by mentor. 
+    Get proper label when student is added by mentor.
 
     Returns:
         name, surname, login, email - string
@@ -48,4 +48,3 @@ def data_get_student_index():
 def index_doesnt_exist():
 
     print("Index does not exist!")
-
